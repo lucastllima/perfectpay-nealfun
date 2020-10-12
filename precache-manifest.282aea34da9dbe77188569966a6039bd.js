12861e4783f04f0cc9cacbd8c73162e882ae4451
lucastllima <lucasteixeira-lima@hotmail.com>
2020-10-12 12:51:44 -0300

    Initial dist subtree commit

---
+++ precache-manifest.282aea34da9dbe77188569966a6039bd.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e6560b10ec382f0862",
+    "url": "/perfectpay-nealfun/css/app.68bff73a.css"
+  },
+  {
+    "revision": "6f1fcb03400a3521c031",
+    "url": "/perfectpay-nealfun/css/chunk-vendors.2aa73d1f.css"
+  },
+  {
+    "revision": "8e502ce6fb0a9aad35f113153897ddb9",
+    "url": "/perfectpay-nealfun/img/acre-of-farmland.8e502ce6.jpg"
+  },
+  {
+    "revision": "2c8a0d403311a9a0cee8311dd2611a45",
+    "url": "/perfectpay-nealfun/img/air-jordans.2c8a0d40.jpg"
+  },
+  {
+    "revision": "2da56b64261c0b085728dea612959a15",
+    "url": "/perfectpay-nealfun/img/airpods.2da56b64.jpg"
+  },
+  {
+    "revision": "3e68bd2d28f7a4c121e2c1924541700e",
+    "url": "/perfectpay-nealfun/img/amazon-echo.3e68bd2d.jpg"
+  },
+  {
+    "revision": "1ac2b74bb80f85e42fde2002a181fb79",
+    "url": "/perfectpay-nealfun/img/apache-helicopter.1ac2b74b.jpg"
+  },
+  {
+    "revision": "b8cc036694710bbb315fad4641f859dc",
+    "url": "/perfectpay-nealfun/img/auto-rickshaw.b8cc0366.jpg"
+  },
+  {
+    "revision": "49746912404407b72f2362dbf9db225e",
+    "url": "/perfectpay-nealfun/img/big-mac.49746912.jpg"
+  },
+  {
+    "revision": "419ff241761c57e757655964517186cc",
+    "url": "/perfectpay-nealfun/img/bike.419ff241.jpg"
+  },
+  {
+    "revision": "226320ceaf16b4148b0d3488746ce2f7",
+    "url": "/perfectpay-nealfun/img/billgates.226320ce.jpg"
+  },
+  {
+    "revision": "240bb31635d988262ee94ceddf4db583",
+    "url": "/perfectpay-nealfun/img/boeing-747.240bb316.jpg"
+  },
+  {
+    "revision": "6436a29a1511b714816f989b30cd8b4b",
+    "url": "/perfectpay-nealfun/img/book.6436a29a.jpg"
+  },
+  {
+    "revision": "ce68daaa6faa0162bf5a22c6398f5ad5",
+    "url": "/perfectpay-nealfun/img/coca-cola-pack.ce68daaa.jpg"
+  },
+  {
+    "revision": "72bf3436d4e7a4b0d64eda5f0cb071c9",
+    "url": "/perfectpay-nealfun/img/cruise-ship.72bf3436.jpg"
+  },
+  {
+    "revision": "b0cc43eb2d3dc24f66d2fed857da440a",
+    "url": "/perfectpay-nealfun/img/designer-handbag.b0cc43eb.jpg"
+  },
+  {
+    "revision": "f7f02a63ddb67d5937d7a7c7a1e65fec",
+    "url": "/perfectpay-nealfun/img/diamond-ring.f7f02a63.jpg"
+  },
+  {
+    "revision": "6d721f621f59b67538bb6cd137c5db65",
+    "url": "/perfectpay-nealfun/img/drone.6d721f62.jpg"
+  },
+  {
+    "revision": "744ffddc53d4d1a55be10a4c76067888",
+    "url": "/perfectpay-nealfun/img/ferrari.744ffddc.jpg"
+  },
+  {
+    "revision": "83c970771df5a6cb3aa8e81eaa2e6683",
+    "url": "/perfectpay-nealfun/img/flip-flops.83c97077.jpg"
+  },
+  {
+    "revision": "80192486e10a18ea39dd1855ca935b4b",
+    "url": "/perfectpay-nealfun/img/ford-f-150.80192486.jpg"
+  },
+  {
+    "revision": "ce63a85d6a09e251b88323c370f044ea",
+    "url": "/perfectpay-nealfun/img/formula-1-car.ce63a85d.jpg"
+  },
+  {
+    "revision": "dae93f8fc6ca729d0da035eaa006a729",
+    "url": "/perfectpay-nealfun/img/gaming-console.dae93f8f.jpg"
+  },
+  {
+    "revision": "e3ee9241792bce06d0ac05f2e7d34e4b",
+    "url": "/perfectpay-nealfun/img/gold-bar.e3ee9241.jpg"
+  },
+  {
+    "revision": "9fc302587e044e324c5a9eb722bfbc2e",
+    "url": "/perfectpay-nealfun/img/horse.9fc30258.jpg"
+  },
+  {
+    "revision": "806f9339f39c226083f7445d4219706b",
+    "url": "/perfectpay-nealfun/img/hot-tub.806f9339.jpg"
+  },
+  {
+    "revision": "e770f138a960d15465e7f2d634694495",
+    "url": "/perfectpay-nealfun/img/jet-ski.e770f138.jpg"
+  },
+  {
+    "revision": "cdda7564b8d7db8c5b673f323ca62f45",
+    "url": "/perfectpay-nealfun/img/kitten.cdda7564.jpg"
+  },
+  {
+    "revision": "d8b277301837032ce3167658507444ea",
+    "url": "/perfectpay-nealfun/img/lobster-dinner.d8b27730.jpg"
+  },
+  {
+    "revision": "4d232bdb8965ada121c414448a210b89",
+    "url": "/perfectpay-nealfun/img/m1-abrams.4d232bdb.jpg"
+  },
+  {
+    "revision": "7038df97c58a5410180ab09163462c7c",
+    "url": "/perfectpay-nealfun/img/make-a-movie.7038df97.jpg"
+  },
+  {
+    "revision": "8993d1bbb69319e0d9af2f3bc447418b",
+    "url": "/perfectpay-nealfun/img/mansion.8993d1bb.jpg"
+  },
+  {
+    "revision": "09158a6295d889d9b4f91132c3a24298",
+    "url": "/perfectpay-nealfun/img/mcdonalds-franchise.09158a62.jpg"
+  },
+  {
+    "revision": "836f571cd722501ade55e77e2a8d802e",
+    "url": "/perfectpay-nealfun/img/mona-lisa.836f571c.jpg"
+  },
+  {
+    "revision": "5e27a2558225fe226df1c7710524c75b",
+    "url": "/perfectpay-nealfun/img/monster-truck.5e27a255.jpg"
+  },
+  {
+    "revision": "2a02b24576c3b9282bd70f4305dc7aeb",
+    "url": "/perfectpay-nealfun/img/movie-ticket.2a02b245.jpg"
+  },
+  {
+    "revision": "55949312941216517bcafdafb977cb8f",
+    "url": "/perfectpay-nealfun/img/nba-team.55949312.jpg"
+  },
+  {
+    "revision": "0d6c757dc604a7d3488543fb787b3cba",
+    "url": "/perfectpay-nealfun/img/puppy.0d6c757d.jpg"
+  },
+  {
+    "revision": "7fb5f6342ec80869c513a2b37ac3aaa6",
+    "url": "/perfectpay-nealfun/img/rolex.7fb5f634.jpg"
+  },
+  {
+    "revision": "8c243c48db2d243d9381407777d55e51",
+    "url": "/perfectpay-nealfun/img/single-family-home.8c243c48.jpg"
+  },
+  {
+    "revision": "2910d986af5306550dc23cf07033ab1d",
+    "url": "/perfectpay-nealfun/img/skyscraper.2910d986.jpg"
+  },
+  {
+    "revision": "489057c8730d19117ceba96d57f5295c",
+    "url": "/perfectpay-nealfun/img/smartphone.489057c8.jpg"
+  },
+  {
+    "revision": "a4e21bb9c101dd4c21cd51382e42aa7f",
+    "url": "/perfectpay-nealfun/img/spend_deprecated.a4e21bb9.png"
+  },
+  {
+    "revision": "40e21b9cb2e9ab4bdfed1d2542b9825c",
+    "url": "/perfectpay-nealfun/img/superbowl-ad.40e21b9c.jpg"
+  },
+  {
+    "revision": "9137895813e766c2208721867f51a390",
+    "url": "/perfectpay-nealfun/img/tesla.91378958.jpg"
+  },
+  {
+    "revision": "61fc992f435a9f8fe683b6b59ce94430",
+    "url": "/perfectpay-nealfun/img/video-game.61fc992f.jpg"
+  },
+  {
+    "revision": "f97961d8bc5fb67d9b845f19d064d365",
+    "url": "/perfectpay-nealfun/img/yacht.f97961d8.jpg"
+  },
+  {
+    "revision": "5453dbcd9db643cd2b13605c0cdd3af0",
+    "url": "/perfectpay-nealfun/img/year-of-netflix.5453dbcd.jpg"
+  },
+  {
+    "revision": "723e845ebdbef8dfe96d349c5ec079b6",
+    "url": "/perfectpay-nealfun/index.html"
+  },
+  {
+    "revision": "c2e6560b10ec382f0862",
+    "url": "/perfectpay-nealfun/js/app.2b36a05c.js"
+  },
+  {
+    "revision": "6f1fcb03400a3521c031",
+    "url": "/perfectpay-nealfun/js/chunk-vendors.54be74e0.js"
+  },
+  {
+    "revision": "d151ef3d6d4a06b3cab2f9d5a325ac7d",
+    "url": "/perfectpay-nealfun/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/perfectpay-nealfun/robots.txt"
+  }
+]);
